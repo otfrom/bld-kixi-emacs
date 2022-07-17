aa03f96837955fda065c57e6a679a99140b731fa
Bruce Durling <bruce@mastodonc.com>
2022-07-17 18:13:45 +0100

    Fiat lux

---
+++ init.el
@@ -0,0 +1,20 @@
+;; Added early-init.el
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+;; (setq straight-use-package-by-default t)
+
+(use-package kixi-emacs
+  :straight (kixi-emacs :type git :repo "https://github.com/MastodonC/kixi-emacs.git"))
