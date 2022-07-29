50a5de089fd75208332913f35db9dd58b6d3bca3
Bruce Durling <bruce@mastodonc.com>
2022-07-29 17:47:19 +0100

    Read things from the internet quickly
    without surveillance

--- init.el
+++ init.el
@@ -277,6 +277,34 @@
 (use-package pass
   :straight t)
 
+(use-package pocket-reader
+  :straight t)
+
+(use-package spray
+  :straight t
+  :commands spray-mode
+  :config 
+  (setq spray-wpm 600
+        spray-height 800)
+  (defun spray-mode-hide-cursor ()
+    "Hide or unhide the cursor as is appropriate."
+    (if spray-mode
+        (setq-local spray--last-evil-cursor-state evil-normal-state-cursor
+                    evil-normal-state-cursor '(nil))
+      (setq-local evil-normal-state-cursor spray--last-evil-cursor-state)))
+  (add-hook 'spray-mode-hook #'spray-mode-hide-cursor)
+  (evil-define-key 'normal spray-mode-map
+    "<return>" 'spray-start/stop
+    "f" 'spray-faster
+    "s" 'spray-slower
+    "t" 'spray-time
+    "<right>" 'spray-forward-word
+    "h" 'spray-forward-word
+    "<left>" 'spray-backward-word
+    "l" 'spray-backward-word
+    "q" 'spray-quit))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; stuff that might migrate to kixi-emacs
 
