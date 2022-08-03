d711d81841d43177a7e4e8df89716ee1a4bade0c
Bruce Durling <bruce@mastodonc.com>
2022-08-03 16:55:32 +0100

    Add consult flyspell

--- init.el
+++ init.el
@@ -343,6 +343,18 @@
   :hook ((text-mode . (lambda () (flyspell-mode 1)))
          (prog-mode . (lambda () (flyspell-prog-mode)))))
 
+(use-package flyspell-correct
+  :straight t
+  :after flyspell)
+
+(use-package consult-flyspell
+  :straight (consult-flyspell :type git :host gitlab :repo "OlMon/consult-flyspell" :branch "master")
+  :config
+  ;; default settings
+  (setq consult-flyspell-select-function nil
+        consult-flyspell-set-point-after-word t
+        consult-flyspell-always-check-buffer nil))
+
 (use-package highlight-symbol
   :straight t
   :defer t
