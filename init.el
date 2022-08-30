dd0e68925dfdd7e2e689d6b0aa8069da7c40a0b5
Bruce Durling <bruce@mastodonc.com>
2022-08-30 15:30:52 +0100

    Ugh. Custom

--- init.el
+++ init.el
@@ -406,3 +406,31 @@
 (server-start)
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-roam-mode-sections
+   (list #'org-roam-backlinks-section #'org-roam-reflinks-section #'org-roam-unlinked-references-section) nil nil "Customized with use-package org-roam")
+ '(safe-local-variable-values
+   '((elisp-lint-indent-specs
+      (describe . 1)
+      (it . 1)
+      (thread-first . 0)
+      (cl-flet . 1)
+      (cl-flet* . 1)
+      (org-element-map . defun)
+      (org-roam-dolist-with-progress . 2)
+      (org-roam-with-temp-buffer . 1)
+      (org-with-point-at . 1)
+      (magit-insert-section . defun)
+      (magit-section-case . 0)
+      (org-roam-with-file . 2))
+     (elisp-lint-ignored-validators "byte-compile" "package-lint"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blamer-face ((t :foreground "#7a88cf" :background nil :height 100 :italic t))))
